28d64deb63c63d19f68c3d088577618a2be77065
Artem <derevets.artem@gmail.com>
2019-01-09 00:18:36 +0200

    rabbitmq init configuration

--- src/main/java/com/gmail/derevets/artem/config/rabbit/RabbitMqConfig.java
+++ src/main/java/com/gmail/derevets/artem/config/rabbit/RabbitMqConfig.java
@@ -2,29 +2,40 @@ package com.gmail.derevets.artem.config.rabbit;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 @Slf4j
 @Configuration
 public class RabbitMqConfig {
-
-    @Autowired
-    private ConnectionFactory connectionFactory;
+    @Value("${rabbitmq.url}")
+    private String rabbitMqUrl;
 
     @Bean
-    public AmqpAdmin amqpAdmin() {
-        return new RabbitAdmin(connectionFactory);
+    public ConnectionFactory connectionFactory() throws URISyntaxException {
+        URI uri = new URI(rabbitMqUrl);
+        return new CachingConnectionFactory(uri);
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate() {
-        return new RabbitTemplate(connectionFactory);
+    public AmqpAdmin amqpAdmin() throws URISyntaxException {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() throws URISyntaxException {
+        return new RabbitTemplate(connectionFactory());
     }
 }
